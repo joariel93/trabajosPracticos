02daede3763a9eb1a5a97767a9c92b80a0f99096
joariel93 <52418439+joariel93@users.noreply.github.com>
2019-09-04 22:16:27 -0300

    Add files via upload

--- trabajo_practico_1/src/trabajo_practico_1.c
+++ trabajo_practico_1/src/trabajo_practico_1.c
@@ -2,52 +2,64 @@
 #include <stdlib.h>
 #include "utn_operaciones.h"
 
+#define NUMBERS 2
+#define RESULT 6
 int main (void){
-float firstNumber;
-float secondNumber;
-float sumResult;
-float subtractionResult;
-float multiplicationResult;
-float divisionResult;
-float factorialResultA;
-float factorialResultB;
+float number[NUMBERS];
+float result[RESULT];
 int choice;
+char seguir1;
+char seguir2;
 
-utn_pideDosNumeros(&firstNumber,&secondNumber);
-utn_sumar(firstNumber,secondNumber,&sumResult);
-utn_restar(firstNumber,secondNumber,&subtractionResult);
-utn_multiplicar(firstNumber,secondNumber,&multiplicationResult);
-if(secondNumber!=0)
+do
 {
-	utn_dividir(firstNumber,secondNumber,&divisionResult);
+utn_pideDosNumeros(&number[0],&number[1]);
+utn_sumar(number[0],number[1],&result[0]);
+utn_restar(number[0],number[1],&result[1]);
+utn_multiplicar(number[0],number[1],&result[2]);
+if(number[1]!=0)
+{
+	utn_dividir(number[0],number[1],&result[3]);
 }
-utn_factorizar2Numeros(firstNumber,secondNumber,&factorialResultA,&factorialResultB);
+utn_factorizar2Numeros(number[0],number[1],&result[4],&result[5]);
 
-printf("Los números ingresados han sido: %.2f y %.2f\n",firstNumber,secondNumber);
-printf("Que operación desea realizar?\n[1]Sumar\n[2]Restar\n[3]Multiplicar\n[4]Dividir\n[5]Factorizar");
-__fpurge(stdin);
-scanf("%d",&choice);	
-switch
-{
-	case 1:	printf("La suma de los números es: %.2f\n",sumResult);
-		break;
-	case 2:	printf("La resta del primero con el segundo es: %.2f\n",subtractionResult);
-		break;
-	case 3:	printf("La mulitplicacion de ambos números es: %.2f\n",multiplicationResult);
-		break;
-	case 4:	if(secondNumber==0)
+	do
+	{
+		printf("Los números ingresados han sido: %.2f y %.2f\n",number[0],number[1]);
+		printf("Que operación desea realizar?\n[1]Sumar\n[2]Restar\n[3]Multiplicar\n[4]Dividir\n[5]Factorizar\n");
+		__fpurge(stdin);
+		scanf("%d",&choice);
+
+		switch(choice)
 		{
-			printf("No es posible dividir por cero.\n");
-			break;
+			case 1:	printf("La suma de los números es: %.2f\n",result[0]);
+					break;
+			case 2:	printf("La resta del primero con el segundo es: %.2f\n",result[1]);
+					break;
+			case 3:	printf("La mulitplicacion de ambos números es: %.2f\n",result[2]);
+					break;
+			case 4:	if(number[1]==0)
+					{
+					printf("No es posible dividir por cero.\n");
+					break;
+					}
+					else
+					{
+					printf("La división del primer numero por el segundo es: %.2f\n",result[3]);
+					break;
+					}
+			case 5:	printf("El factorial del primer número es: %.2f y del segundo es: %.2f\n",result[4],result[5]);
+					break;
+			default:printf("Opción invalida.");
+					break;
 		}
-		else
-		{
-			printf("La división del primer numero por el segundo es: %.2f\n",divisionResult);
-			break;
-		}
-	case 5:	printf("El factorial del primer número es: %.2f y del segundo es: %.2f\n",factorialResultA,factorialResultB);
-		break;
-	default: break;	
-}
+		printf("Desea realizar otra operacion con los mismos numeros? s/n");
+		__fpurge(stdin);
+		scanf("%c",&seguir1);
+	}while(seguir1!='n');
+	printf("Desea realizar otra operacion con otros numeros? s/n");
+	__fpurge(stdin);
+	scanf("%c",&seguir2);
+}while(seguir2!='n');
 return 0;
 }
