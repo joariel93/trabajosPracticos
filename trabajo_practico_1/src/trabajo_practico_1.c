8c1a80a9848e3eec4b8c46a4c56b71d061a9ba06
joariel93 <52418439+joariel93@users.noreply.github.com>
2019-09-04 21:46:57 -0300

    Update trabajo_practico_1.c

--- trabajo_practico_1/src/trabajo_practico_1.c
+++ trabajo_practico_1/src/trabajo_practico_1.c
@@ -11,6 +11,7 @@ float multiplicationResult;
 float divisionResult;
 float factorialResultA;
 float factorialResultB;
+int choice;
 
 utn_pideDosNumeros(&firstNumber,&secondNumber);
 utn_sumar(firstNumber,secondNumber,&sumResult);
@@ -23,17 +24,30 @@ if(secondNumber!=0)
 utn_factorizar2Numeros(firstNumber,secondNumber,&factorialResultA,&factorialResultB);
 
 printf("Los números ingresados han sido: %.2f y %.2f\n",firstNumber,secondNumber);
-printf("La suma de los números es: %.2f\n",sumResult);
-printf("La resta del primero con el segundo es: %.2f\n",subtractionResult);
-printf("La mulitplicacion de ambos números es: %.2f\n",multiplicationResult);
-if(secondNumber==0)
+printf("Que operación desea realizar?\n[1]Sumar\n[2]Restar\n[3]Multiplicar\n[4]Dividir\n[5]Factorizar");
+__fpurge(stdin);
+scanf("%d",&choice);	
+switch
 {
-	printf("No es posible dividir por cero.\n");
+	case 1:	printf("La suma de los números es: %.2f\n",sumResult);
+		break;
+	case 2:	printf("La resta del primero con el segundo es: %.2f\n",subtractionResult);
+		break;
+	case 3:	printf("La mulitplicacion de ambos números es: %.2f\n",multiplicationResult);
+		break;
+	case 4:	if(secondNumber==0)
+		{
+			printf("No es posible dividir por cero.\n");
+			break;
+		}
+		else
+		{
+			printf("La división del primer numero por el segundo es: %.2f\n",divisionResult);
+			break;
+		}
+	case 5:	printf("El factorial del primer número es: %.2f y del segundo es: %.2f\n",factorialResultA,factorialResultB);
+		break;
+	default: break;	
 }
-else
-{
-	printf("La división del primer numero por el segundo es: %.2f\n",divisionResult);
-}
-printf("El factorial del primer número es: %.2f y del segundo es: %.2f\n",factorialResultA,factorialResultB);
 return 0;
 }
